c2f3e9a493ac674a8d8b1c1fa0b757848d4af8e9
rcerney <ryancerney1@gmail.com>
2025-04-22 16:20:45 -0700

    vite build

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-      allowedHosts: true
+    allowedHosts: true
+  },
+  build: {
+    outDir: 'dist', // Specify the output directory for the build
   },
 })
