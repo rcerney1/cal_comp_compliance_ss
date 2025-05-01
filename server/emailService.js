61707df35acec72de501e0f525d8083b2d61b937
rcerney <ryancerney1@gmail.com>
2025-05-01 15:13:51 -0700

    email setup

--- server/emailService.js
+++ server/emailService.js
@@ -5,20 +5,24 @@ require('dotenv').config();
 
 // Create a transporter for nodemailer (configure your email service here)
 const transporter = nodemailer.createTransport({
-    service: 'gmail', 
+    host: 'smtp.porkbun.com',
+    port: 587,
+    secure: false, // false for STARTTLS
     auth: {
-        user: process.env.EMAIL_USER,  
-        pass: process.env.EMAIL_PASS,   
+        user: process.env.EMAIL_USER, // mike@californiacompliancesecurity.com
+        pass: process.env.EMAIL_PASS, // Your email password
+    },
+    tls: {
+        ciphers: 'SSLv3',
     },
 });
+
+
 
 // Create a function to send an email
 const sendEmail = async (fromEmail, subject, text) => {
     
     
-    
-
-
     const mailOptions = {
         from: fromEmail, 
         to: process.env.EMAIL_USER, 
