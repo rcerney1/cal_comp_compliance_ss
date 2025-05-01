1f98e778d2629ef377849b18fee3ba432accb7d1
rcerney <ryancerney1@gmail.com>
2025-04-30 18:34:35 -0700

    env

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -9,7 +9,7 @@ function Contact() {
     const backendUrl = process.env.REACT_APP_BACKEND_URL
     //const backendUrl = 'http://localhost:5000'  
     console.log('is this going through: ', backendUrl)
-    console.log('loading environment variables: ', process.env)
+    console.log('loading environment variables: ', process.env.EMAIL_USER)
 
     useEffect(() => {
         window.scrollTo(0, 0)
