ad9ba57848e13968d1f72eee2929d57218311fa6
rcerney <ryancerney1@gmail.com>
2025-06-17 10:50:54 -0700

    took out gallery

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/projects" element={<Gallery />} />
+        {/* <Route path="/projects" element={<Gallery />} /> */}
         
       </Routes>
     </Router>
